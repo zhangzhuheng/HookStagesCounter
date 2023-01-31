5c87c1c692ba012ae676dfd7a892f1702ad50f29
zhangzhuheng <zhangzhuheng2018@gmail.com>
2023-01-31 21:11:40 +0800

    :sparkles: 按下Alt可以移动窗口位置和修改大小

--- HookWindow.py
+++ HookWindow.py
@@ -1,7 +1,9 @@
+import contextlib
 from base64 import b64decode
 from ctypes import windll
 from io import BytesIO
 from tkinter import Tk, Frame, Label, constants, PhotoImage
+from _tkinter import TclError
 from PIL import ImageTk, Image
 from win32con import WS_EX_TOOLWINDOW, WS_EX_APPWINDOW, GWL_EXSTYLE
 from queue import Queue, Empty
@@ -12,10 +14,12 @@ HOOK_IMG_BASE64 = 'iVBORw0KGgoAAAANSUhEUgAAAFcAAAB4CAYAAACHMm6PAAAAAXNSR0IArs4c6
 
 class HookWindow:
     def __init__(self, messages: Queue):
+        self.start_abs_x, self.start_abs_y, self.start_width, self.start_height = None, None, None, None
         self._messages = messages
 
         self._root = Tk()
         self._root.title('HookWindow')
+        self._root.minsize(50, 300)  # 设置窗口最小值
         self._root.iconphoto(True, PhotoImage(data=b64decode(HOOK_ICO_BASE64)))
         self._root.overrideredirect(True)  # 设置隐藏窗口标题栏和任务栏图标
         self._root.attributes('-topmost', True)
@@ -28,22 +32,74 @@ class HookWindow:
         # print(f'{self._root_x=}, {self._root_y=}, {self._root_width=}, {self._root_height=}')
         self._root.geometry(f'{self._root_width}x{self._root_height}+{self._root_x}+{self._root_y}')
 
-        frame = Frame(self._root, bg='black')
-        frame.pack(side=constants.TOP, fill=constants.BOTH, expand=1)
-
         hook_img_width = self._root_width // 2
         hook_img_height = hook_img_width * 4 // 3
         self.hook_img = ImageTk.PhotoImage(
             Image.open(BytesIO(b64decode(HOOK_IMG_BASE64))).resize((hook_img_width, hook_img_height)))
 
+        frame = Frame(self._root, bg='black')
+        frame.pack(side=constants.TOP, fill=constants.BOTH, expand=1)
         self.hooks = [[Label(frame, background='black') for _ in range(2)] for _ in range(4)]
         self.counter = [0, 0, 0, 0]
         self.layout_hook()
+
+        self.post_label = Label(self._root, bg='#2980b9')
+        self.post_label.bind('<Button-1>', self._get_point)
+        self.post_label.bind("<B1-Motion>", self._motion_call)
+
+        self.size_label = [Label(self._root, bg='#27ae60'), Label(self._root, bg='#27ae60')]
+        self.size_label[0].bind("<B1-Motion>", self._zoom_call)
+        self.size_label[1].bind("<B1-Motion>", self._zoom_call)
+
+        self._root.bind('<KeyPress-Alt_L>', self._alt_press_call)
+        self._root.bind('<KeyRelease-Alt_L>', self._alt_release_call)
+        self._root.bind('<ButtonPress-1>', self._start_drag)
 
     def layout_hook(self):
         for i in range(len(self.hooks)):
             self.hooks[i][0].place(anchor=constants.W, relx=0, rely=(0.1 + 0.25 * i))
             self.hooks[i][1].place(anchor=constants.W, relx=0.5, rely=(0.1 + 0.25 * i))
+
+    def _get_point(self, event):
+        """获取当前窗口位置并保存"""
+        self._root_x, self._root_y = event.x, event.y
+
+    def _motion_call(self, event):
+        """窗口移动事件"""
+        new_x = (event.x - self._root_x) + self._root.winfo_x()
+        new_y = (event.y - self._root_y) + self._root.winfo_y()
+        self._root.geometry(f'{self._root.winfo_width()}x{self._root.winfo_height()}+{new_x}+{new_y}')
+
+    def _start_drag(self, event):
+        self._root.update_idletasks()
+        self.start_abs_x = self._root.winfo_pointerx() - self._root.winfo_rootx()
+        self.start_abs_y = self._root.winfo_pointery() - self._root.winfo_rooty()
+        self.start_width = self._root.winfo_width()
+        self.start_height = self._root.winfo_height()
+
+    def _zoom_call(self, event):
+        self._root.update_idletasks()
+        end_abs_x = self._root.winfo_pointerx() - self._root.winfo_rootx()
+        end_abs_y = self._root.winfo_pointery() - self._root.winfo_rooty()
+        calc_w = self.start_width + (end_abs_x - self.start_abs_x)
+        calc_h = self.start_height + (end_abs_y - self.start_abs_y)
+
+        with contextlib.suppress(TclError):
+            self._root.geometry(f'{calc_w}x{calc_h}+{self._root.winfo_x()}+{self._root.winfo_y()}')
+
+    def _alt_press_call(self, event):
+        self._root.attributes('-transparentcolor', '')
+        self._root.attributes('-alpha', 0.5)
+        self.post_label.place(relwidth=1, height=10, relx=0, rely=0, anchor=constants.NW)
+        self.size_label[0].place(width=5, height=15, relx=1, rely=1, anchor=constants.SE)
+        self.size_label[1].place(width=15, height=5, relx=1, rely=1, anchor=constants.SE)
+
+    def _alt_release_call(self, event):
+        self._root.attributes('-transparentcolor', 'black')
+        self._root.attributes('-alpha', 1)
+        self.post_label.place_forget()
+        self.size_label[0].place_forget()
+        self.size_label[1].place_forget()
 
     def reset_hook(self):
         for l1, l2 in self.hooks:
