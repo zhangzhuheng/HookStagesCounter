70c616f5206520ef1226125ed3651b4234f6121c
zhangzhuheng <zhangzhuheng2018@gmail.com>
2023-01-31 21:18:58 +0800

    :zap: 将通过队列传递消息改进为使用回调函数

--- HookStagesTool.py
+++ HookStagesTool.py
@@ -1,15 +1,10 @@
-from queue import Queue
+from HookWindow import HookWindow
 from keyboard import on_press_key
 
-from HookWindow import HookWindow
-
 if __name__ == '__main__':
-
-    messages = Queue()
-    hw = HookWindow(messages)
-    on_press_key('1', lambda event: messages.put(event))
-    on_press_key('2', lambda event: messages.put(event))
-    on_press_key('3', lambda event: messages.put(event))
-    on_press_key('4', lambda event: messages.put(event))
+    hw = HookWindow()
+    on_press_key('1', lambda event: hw.show_hook(0) if event.name == '1' else hw.hide_hook(0))
+    on_press_key('2', lambda event: hw.show_hook(1) if event.name == '2' else hw.hide_hook(1))
+    on_press_key('3', lambda event: hw.show_hook(2) if event.name == '3' else hw.hide_hook(2))
+    on_press_key('4', lambda event: hw.show_hook(3) if event.name == '4' else hw.hide_hook(3))
     hw.run()
-
